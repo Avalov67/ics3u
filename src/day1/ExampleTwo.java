f05a1b4cc273a7e5eba126e6ef6491ce2e413b1c
Alexandra.v09 <avalov@bayviewglen.ca>
2024-09-16 08:46:49 -0400

    Day 1 Examples

---
+++ src/day1/ExampleTwo.java
@@ -0,0 +1,13 @@
+package day1;           // semi-colon is the end of a statment 
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class ExampleTwo {           // {} are used to indicate something is embedded inside something else
+    public static void main(String[] args) {
+        System.out.println("this is harder than python");
+        System.out.println("Sysout makes it faster");       // use sysout as a shortcut
+        
+        System.out.print("A");
+        System.out.println("B");
+    }
+}
